f06beb369bd92a74a7456144198276562abe9ba9
Ardian <168179647+ardiianar@users.noreply.github.com>
2024-06-10 21:20:36 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Invoice invoice1 = new Invoice("Dompet", 1, 1000000);
         Invoice invoice2 = new Invoice("Termos", 1, 900000);
         Invoice[] invoices = {invoice1, invoice2};
-        Employee employee = new Employee(101010, "Fardan",
+        Employee employee = new Employee(101010, "Ardian",
                 7500000, invoices);
         System.out.println("--------------------------------------------");
         System.out.println("Data Karyawan \n");
